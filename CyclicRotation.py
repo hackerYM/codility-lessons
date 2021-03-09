c96fc783a6f7d12a67a8ebd989420fc34f1445a2
hackerYM <hacker.yenming@gmail.com>
2021-03-10 00:22:51 +0800

    feat(all): update the practices from lesson 1 - 4

--- CyclicRotation.py
+++ CyclicRotation.py
@@ -1,7 +1,7 @@
 def solution(A, K):
-    
+
     if len(A) == 0:
         return []
-    
+
     N = K % len(A)
     return A[-N:] + A[:-N]
--- FrogRiverOne.py
+++ FrogRiverOne.py
@@ -1,10 +1,10 @@
 def solution(X, A):
-    
+
     num_set = set()
-    
+
     for idx, num in enumerate(A):
         num_set.add(num)
         if len(num_set) == X:
             return idx
-    
+
     return -1
