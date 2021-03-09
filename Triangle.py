f9840eb4bc397854009a6909da0b1a88f359db0e
hackerYM <hacker.yenming@gmail.com>
2021-03-10 00:24:30 +0800

    feat(all): update the practices from lesson 5 - 6

--- Triangle.py
+++ Triangle.py
@@ -1,12 +1,12 @@
 def solution(A):
-    
+
     if len(A) < 3:
         return 0
-        
+
     A.sort()
-    
+
     for idx in range(0, len(A)-2):
         if A[idx] + A[idx+1] > A[idx+2]:
             return 1
-    
+
     return 0
